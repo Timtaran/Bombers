4c681fb26755257779ada28c8b14dadd89c2966e
Timtaran <tentaranka@gmail.com>
2021-03-26 11:25:46 +0000

    1.1.2

---
+++ duplo-bomber.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/batiscuff/duplo-bomber
+pip3 install -r duplo-bomber/requirements.txt
+clear
+python3 duplo-bomber/duplo_spam.py
--- select.py
+++ select.py
@@ -1,9 +1,15 @@
-import os
-bomber=input("Select bomber\n0-b0mb3r by Denishnc|Don't working\n1-Impulse by LimerBoy\n2-Spammer by cludeex\n sh start_bomber.sh ")
+import os, requests
+version_on_pc=open('version.txt', 'r')
+version=version_on_pc.read()
+version_on_pc.close()
+github_version=requests.get('https://raw.githubusercontent.com/Timtaran/Bombers/master/version.txt').text
+if github_version != version:
+  print(f'Please update "Bombers"\nYour version - {version}, GitHub version - {github_version}')
+bomber=input("Select bomber\n0-b0mb3r by Denishnc|Don't working\n1-Impulse by LimerBoy\n2-Spammer by Cludeex\n3 Duplo-bomber by Batiscuff \n sh start_bomber.sh ")
 os.system('clear')
 bomber=int(bomber[0])
-bomders=['sh b0mb3r.sh', 'sh Impulse.sh', 'python3 spammer.py']
-bomders_enter=['', 'Impulse.sh --target [number]', '']
+bomders=['sh b0mb3r.sh', 'sh Impulse.sh', 'python3 spammer.py', 'sh duplo-bomber.sh']
+bomders_enter=['', 'Impulse.sh --target [number]', '', '']
 if bomders_enter[bomber] != '':
   input(f"Number Selecting - {bomders_enter[bomber]}\n Press Enter To Continue...")
 os.system(bomders[bomber])
